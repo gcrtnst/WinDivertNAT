15311f1a4e5f972f57b211c27ddbc0f9d2768a94
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2021-04-30 16:03:24 +0900

    Fix license notices

--- WinDivertNAT/NativeMethods.cs
+++ WinDivertNAT/NativeMethods.cs
@@ -15,7 +15,7 @@
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with WinDivertNAT.  If not, see <http://www.gnu.org/licenses/>.
  *
  * WinDivertNAT is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -28,7 +28,7 @@
  * for more details.
  * 
  * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * with WinDivertNAT; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
