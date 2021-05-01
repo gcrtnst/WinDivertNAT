a194276339859f8867f78da2304a6536911604ba
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2021-05-01 15:39:10 +0900

    Remove unnecessary InAttribute

--- WinDivertNAT/NativeMethods.cs
+++ WinDivertNAT/NativeMethods.cs
@@ -40,7 +40,7 @@ namespace WinDivertNAT
     internal static class NativeMethods
     {
         [DllImport("WinDivert.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi, BestFitMapping = false, ThrowOnUnmappableChar = true, PreserveSig = true, SetLastError = true)]
-        public static extern IntPtr WinDivertOpen([In] string filter, WinDivertConstants.WinDivertLayer layer, short priority, WinDivertConstants.WinDivertFlag flags);
+        public static extern IntPtr WinDivertOpen(string filter, WinDivertConstants.WinDivertLayer layer, short priority, WinDivertConstants.WinDivertFlag flags);
 
         [DllImport("WinDivert.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi, BestFitMapping = false, ThrowOnUnmappableChar = true, PreserveSig = true, SetLastError = true)]
         public static extern bool WinDivertSetParam(IntPtr handle, WinDivertConstants.WinDivertParam param, ulong value);
