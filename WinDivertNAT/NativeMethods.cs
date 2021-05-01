e2c3e70293dcdeb2e2da3a1fb1b3a3d93f32952d
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2021-05-01 15:52:33 +0900

    Add WinDivertEvent

--- WinDivertNAT/NativeMethods.cs
+++ WinDivertNAT/NativeMethods.cs
@@ -66,6 +66,20 @@ namespace WinDivertNAT
             Reflect = 4,
         }
 
+        public enum WinDivertEvent
+        {
+            NetworkPacket = 0,
+            FlowEstablished = 1,
+            FlowDeleted = 2,
+            SocketBind = 3,
+            SocketConnect = 4,
+            SocketListen = 5,
+            SocketAccept = 6,
+            SocketClose = 7,
+            ReflectOpen = 8,
+            ReflectClose = 9,
+        }
+
         [Flags]
         public enum WinDivertFlag : ulong
         {
