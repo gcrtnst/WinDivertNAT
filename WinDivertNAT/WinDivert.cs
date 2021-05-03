fa0ac42ca4d822fcc63d18ac6e18891a5cfd4176
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2021-05-03 17:16:38 +0900

    Add PacketParser

--- WinDivertNAT/WinDivert.cs
+++ WinDivertNAT/WinDivert.cs
@@ -33,6 +33,9 @@
  */
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace WinDivertNAT
 {
@@ -73,5 +76,103 @@ namespace WinDivertNAT
         public void ShutdownSend() => WinDivertLow.WinDivertShutdown(handle, WinDivertConstants.WinDivertShutdown.Send);
         public void Shutdown() => WinDivertLow.WinDivertShutdown(handle, WinDivertConstants.WinDivertShutdown.Both);
         public void Dispose() => handle.Dispose();
+    }
+
+    internal struct PacketParser : IEnumerable<ParseResult>
+    {
+        private readonly byte[] packet;
+
+        public PacketParser(byte[] packet)
+        {
+            this.packet = packet;
+        }
+
+        public IEnumerator<ParseResult> GetEnumerator() => new PacketEnumerator(packet);
+        IEnumerator IEnumerable.GetEnumerator() => new PacketEnumerator(packet);
+    }
+
+    internal unsafe class PacketEnumerator : IEnumerator<ParseResult>
+    {
+        private readonly GCHandle gch;
+        private readonly byte[] packet;
+        private readonly byte* pPacket0;
+        private byte* pPacket;
+        private uint packetLen;
+
+        private ParseResult current;
+        public ParseResult Current => current;
+        object IEnumerator.Current => current;
+
+        public PacketEnumerator(byte[] packet)
+        {
+            gch = GCHandle.Alloc(packet, GCHandleType.Pinned);
+            this.packet = packet;
+            pPacket0 = (byte*)gch.AddrOfPinnedObject();
+            Reset();
+        }
+
+        public bool MoveNext()
+        {
+            var ipv4Hdr = (WinDivertIPv4Hdr*)null;
+            var ipv6Hdr = (WinDivertIPv6Hdr*)null;
+            var protocol = (byte)0;
+            var icmpv4Hdr = (WinDivertICMPv4Hdr*)null;
+            var icmpv6Hdr = (WinDivertICMPv6Hdr*)null;
+            var tcpHdr = (WinDivertTCPHdr*)null;
+            var udpHdr = (WinDivertUDPHdr*)null;
+            var pData = (byte*)null;
+            var dataLen = (uint)0;
+            var pNext = (byte*)null;
+            var nextLen = (uint)0;
+
+            var success = NativeMethods.WinDivertHelperParsePacket(pPacket, packetLen, &ipv4Hdr, &ipv6Hdr, &protocol, &icmpv4Hdr, &icmpv6Hdr, &tcpHdr, &udpHdr, (void**)&pData, &dataLen, (void**)&pNext, &nextLen);
+            if (!success) return false;
+
+            current.Packet = pNext != null
+                ? packet[(int)(pPacket - pPacket0)..(int)(pNext - pPacket0)]
+                : packet[(int)(pPacket - pPacket0)..(int)(pPacket + packetLen - pPacket0)];
+            current.IPv4Hdr = ipv4Hdr;
+            current.IPv6Hdr = ipv6Hdr;
+            current.Protocol = protocol;
+            current.ICMPv4Hdr = icmpv4Hdr;
+            current.ICMPv6Hdr = icmpv6Hdr;
+            current.TCPHdr = tcpHdr;
+            current.UDPHdr = udpHdr;
+            current.Data = pData != null && dataLen > 0
+                ? packet[(int)(pData - pPacket0)..(int)(pData + dataLen - pPacket0)]
+                : null;
+
+            pPacket = pNext;
+            packetLen = nextLen;
+            return true;
+        }
+
+        public void Reset()
+        {
+            current = new ParseResult();
+            pPacket = pPacket0;
+            packetLen = (uint)packet.Length;
+        }
+
+        public void Dispose()
+        {
+            gch.Free();
+            GC.SuppressFinalize(this);
+        }
+
+        ~PacketEnumerator() => gch.Free();
+    }
+
+    internal unsafe struct ParseResult
+    {
+        public byte[] Packet;
+        public WinDivertIPv4Hdr* IPv4Hdr;
+        public WinDivertIPv6Hdr* IPv6Hdr;
+        public byte Protocol;
+        public WinDivertICMPv4Hdr* ICMPv4Hdr;
+        public WinDivertICMPv6Hdr* ICMPv6Hdr;
+        public WinDivertTCPHdr* TCPHdr;
+        public WinDivertUDPHdr* UDPHdr;
+        public byte[]? Data;
     }
 }
